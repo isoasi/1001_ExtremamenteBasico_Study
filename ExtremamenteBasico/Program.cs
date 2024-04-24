173656a3f75181a9ae7c1b5ad0cd8eb4226ebcf4
Paola <Paola@DESKTOP-L53JRFL>
2024-04-23 22:30:57 -0300

    Adicionar arquivos de projeto.

---
+++ ExtremamenteBasico/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Globalization;
+
+namespace curso
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, X;   
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            X = A + B;
+            Console.WriteLine("X = " + X);
+
+
+
+        }
+    }
+}
